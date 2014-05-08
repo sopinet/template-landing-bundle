2692ffed7f4555a47654d2832543d17d38062bcf
Sergio Yamuza <syamuza@sopinet.com>
2014-05-08 10:12:55 +0200

    desgin updated

--- Resources/public/js/custom.js
+++ Resources/public/js/custom.js
@@ -1,10 +1,10 @@
 $(function(){
 
 	
-	$(".social").hover(function(){
+	/*$(".social").hover(function(){
 		$(this).addClass("icon-hover",300);	
 	});
 	$(".social").mouseleave(function(){
 		$(this).removeClass("icon-hover",0);	
-	});
+	});*/
 });
